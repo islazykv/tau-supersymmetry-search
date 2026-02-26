56ad628768c740e2ddc94c81abd7f8b852248c47
islazykv <islazykv@gmail.com>
2026-02-26 10:47:28 +0100

    chore: consolidate I/O and clean up configs

--- src/utils.py
+++ src/utils.py
@@ -1,34 +1,7 @@
 import warnings
-from pathlib import Path
-
-import awkward as ak
-import pandas as pd
 
 
 def suppress_warnings():
     """Suppress unessential warnings."""
     warnings.filterwarnings("ignore")
     print("Unessential warnings suppressed.")
-
-
-def save_data(data, output_path: str):
-    """
-    Saves data to Parquet format.
-    Handles both Awkward Arrays and Pandas DataFrames.
-    """
-    path = Path(output_path)
-    path.parent.mkdir(parents=True, exist_ok=True)
-
-    if isinstance(data, pd.DataFrame):
-        data.to_parquet(path)
-    elif isinstance(data, ak.Array):
-        ak.to_parquet(data, path)
-
-    print(f"File saved to: {output_path}")
-
-
-def load_data(file_path: str, is_awkward: bool = False):
-    """Loads data from Parquet format."""
-    if is_awkward:
-        return ak.from_parquet(file_path)
-    return pd.read_parquet(file_path)
